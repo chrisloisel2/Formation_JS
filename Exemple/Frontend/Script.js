18116353fc91ec5b4bdbcdbc714c3260894c7c69
chrisloisel2 <christopher.loisel42@gmail.com>
2023-06-28 15:36:12 +0200

    Exercice

--- Exemple/Frontend/Script.js
+++ Exemple/Frontend/Script.js
@@ -1,10 +1,18 @@
-document.getElementById("button").addEventListener("click", function () {
-    fetch("http://localhost:5555/api/books")
-        .then(function (response) {
+document.getElementById("button").addEventListener("click", getBooks);
+
+function getBooks() {
+    fetch("http://82.165.127.44/users", {
+        methode: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "ACCESS-CONTROL-ALLOW-ORIGIN": "http://localhost:5555",
+        },
+    })
+        .then((response) => {
+            console.log(response);
             return response.json();
         })
-        .then(function (data) {
+        .then((data) => {
             console.log(data);
         });
-    // asynchronous
-});
+}
