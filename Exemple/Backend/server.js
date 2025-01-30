f2c80a1895def2688479c6b97ab429ceec67cdc1
ChristopherLoisel <christo.loisel@gmail.com>
2025-01-30 10:07:20 +0100

    ok

--- Exemple/Backend/server.js
+++ Exemple/Backend/server.js
@@ -6,41 +6,47 @@ const app = express(); // Create express app
 const port = 5555; // definir le port d'écoute
 
 const wss = new WebSocket.Server({ port: 8080 });
+const users = []
+const message = []
 
 wss.on("connection", function connection(ws) {
-    console.log("New client connected");
+	console.log("New client connected");
 
-    ws.on("message", function incoming(message) {
-        console.log("received: %s", message);
-        ws.send("le message a ete recu");
-    });
+	// Ajouter l'utilisateur a la liste d'utilisateur avec son ID
+	const userId = users.length + 1;
+	users.push({ id: userId, ws: ws });
+	console.log(`User ${userId} added`);
+
+	ws.on("message", function incoming(message) {
+		ws.send("le message a ete recu");
+	});
 });
 
 app.use(cors()); // Use cors
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+	console.log(`Server started on port ${port}`);
 });
 
 books = [
-    {
-        id: 1,
-        title: "Harry Potter and the Sorcerer's stone",
-        author: "J.K. Rowling",
-        year: 1997,
-    },
-    {
-        id: 2,
-        title: "Jurassic Park",
-        author: "Michael Crichton",
-        year: 1990,
-    },
-    {
-        id: 3,
-        title: "The Lord of the Rings",
-        author: "J.R.R. Tolkien",
-        year: 1954,
-    },
+	{
+		id: 1,
+		title: "Harry Potter and the Sorcerer's stone",
+		author: "J.K. Rowling",
+		year: 1997,
+	},
+	{
+		id: 2,
+		title: "Jurassic Park",
+		author: "Michael Crichton",
+		year: 1990,
+	},
+	{
+		id: 3,
+		title: "The Lord of the Rings",
+		author: "J.R.R. Tolkien",
+		year: 1954,
+	},
 ];
 
 // GET
@@ -49,6 +55,6 @@ books = [
 // DELETE
 
 app.get("/api/books", (req, res) => {
-    console.log("GET /api/books");
-    res.json(books);
+	console.log("GET /api/books");
+	res.json(books);
 });
